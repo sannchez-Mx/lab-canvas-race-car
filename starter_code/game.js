c0508210c2358e8770d547b395533806ca0565c0
sannchez-Mx <sannchez-mx@outlook.com>
2018-08-24 20:01:15 -0500

    2da actualización

--- starter_code/game.js
+++ starter_code/game.js
@@ -16,5 +16,4 @@ window.onload = function() {
         } 
     }
 
-    class
 };
--- starter_code/script.js
+++ starter_code/script.js
@@ -56,18 +56,44 @@ window.onload = function() {
       }
 
       draw(){
-        if(this.y < 335) this.y += 5;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 
       };
     }
 
+    class Highway{
+      constructor(){
+          this.x = 0;
+          this.y = 0;
+          this.width = canvas.width;
+          this.height = 1000;
+          this.image = new Image();
+          this.image.src = "./images/pista.png";
+        }
+        
+       gameOver(){
+          sonido.pause();
+          clearInterval(interval);
+          ctx.font = "40px Avenir";
+          ctx.fillText("GameOver", 115, 100);
+        }
+  
+      draw(){
+          this.y++;
+          if(this.y < -canvas.width) this.y = 0;
+          ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+          ctx.drawImage(this.image, this.x, this.y - this.height, this.width, this.height);
+        }
+  
+  }
     var car = new Car();
+    var highway = new Highway();
 
     var frames = 0;
     var interval = setInterval(function(){
       frames++;
-      //ctx.clearRect(0,0, 400, 600);
+      ctx.clearRect(0,0, 400, 600);
+      highway.draw();
       car.draw();
       gameArea();
     }, 1000/60);
@@ -75,7 +101,6 @@ window.onload = function() {
     function gameArea() {
       car.newPos();
     }
-
     
     addEventListener("keydown", function(e){
       if(e.keyCode === 37){
